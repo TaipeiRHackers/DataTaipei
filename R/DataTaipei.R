75cb71c74c7abf94ff7cd06f46056b314e31ae5c
Wush Wu <wush978@gmail.com>
2015-05-09 20:27:41 +0800

    basic structure

--- R/DataTaipei.R
+++ R/DataTaipei.R
@@ -2,3 +2,15 @@
 get_url <- function() {
   "http://data.taipei"
 }
+
+query_server <- function(url) {
+  response <- httr::GET(url)
+  httr::stop_for_status(response)
+  result <- httr::content(response)
+  # make the result be a data.frame
+}
+
+#'@export
+print.DataTaipeiResult <- function(x, ...) {
+  # implement the print function
+}
---
+++ R/dataSetMetadataSearch.R
@@ -0,0 +1,7 @@
+#'@title Dataset Metadata Search
+#'@export
+dataSetMetadataSearch <- function(keyword = NULL, filter = character(0), limit = 100L, offset = 0L) {
+  # construct url
+  
+  # use the api in DataTaipei.R to return the result
+}
