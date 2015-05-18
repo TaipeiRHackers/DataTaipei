3ea769e5a59d082b7fc184f4c8280aa47940b59d
Wush Wu <wush978@gmail.com>
2015-05-18 21:36:58 +0800

    reformulate API skema for will121 and john-data-chen

--- R/DataTaipei.R
+++ R/DataTaipei.R
@@ -1,20 +1,6 @@
-#'@export
-get_url <- function() {
-  "http://data.taipei"
+dataTaipeiGET <- function(path, ...) {
 }
 
-dataTaipei_GET <- function(path, ...) {
-  req <- httr::GET("http://data.taipei/opendata", path = path, auth, ...)
-  dataTaipei_check(req)
+dataTaipeiCheck <- function(req) {
 }
 
-dataTaipei_check <- function(req) {
-  httr::stop_for_status(req)
-  dataTaipei_parse(req)
-}
-
-dataTaipei_parse <- function(req) {
-  text <- content(req, as = "text")
-  if (identical(text, "")) stop("Not output to parse", call. = FALSE)
-  jsonlite::fromJSON(text, simplifyVector = FALSE)
-}
---
+++ R/resourceMetadataSearch.R
@@ -0,0 +1,4 @@
+#'@export
+resourceMetadataSearch <- function(...) {
+  
+}
