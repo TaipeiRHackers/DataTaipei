cf28da9e7ab8febc5c8ed90198c5d72df43edd92
will121 <will121@gmail.com>
2015-05-21 21:34:25 +0800

    rewrite dataTaipeiGET() on Wush's advice

--- R/DataTaipei.R
+++ R/DataTaipei.R
@@ -1,20 +1,11 @@
 dataTaipeiGET <- function(scope=NULL,q=NULL,limit=1000,offset=0) {
-  
-  # 輸入查詢範例 dataTaipeiGET("datasetMetadataSearch","youbike")
-  
   url <- "http://data.taipei/opendata/datalist/apiAccess"
-  
-  req <- httr::GET(url,query=list(scope=scope,q=q,offset=offset,limit=limit)) 
-  dataTaipeiCheck(req)
-  
-  if(scope=="datasetMetadataSearch"){
-    dataSetMetadataSearch(req)
-  }else{
-    resourceMetadataSearch(req)
-  }
+  response <- httr::GET(url,query=list(scope=scope,q=q,limit=limit,offset=offset)) 
+  dataTaipeiCheck(response)
+  response
 }
 
-dataTaipeiCheck <- function(req) {
-  httr::stop_for_status(req)
+dataTaipeiCheck <- function(response) {
+  httr::stop_for_status(response)
 }
 
