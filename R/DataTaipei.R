485b55ee057b4679351dda25006daf5b38179781
Wush Wu <wush978@gmail.com>
2015-05-22 22:55:06 +0800

    use dataTaipeiGet

--- R/DataTaipei.R
+++ R/DataTaipei.R
@@ -1,6 +1,6 @@
-dataTaipeiGET <- function(scope=NULL,q=NULL,limit=1000,offset=0) {
+dataTaipeiGET <- function(scope=NULL,q=NULL,limit=1000,offset=0, ...) {
   url <- "http://data.taipei/opendata/datalist/apiAccess"
-  response <- httr::GET(url,query=list(scope=scope,q=q,limit=limit,offset=offset)) 
+  response <- httr::GET(url,query=list(scope=scope,q=q,limit=limit,offset=offset, ...)) 
   dataTaipeiCheck(response)
   response
 }
