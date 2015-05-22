628315d75be522f9f7cd3917622cd863c58796d6
Wush Wu <wush978@gmail.com>
2015-05-22 23:21:48 +0800

    encoding issue

--- R/DataTaipei.R
+++ R/DataTaipei.R
@@ -1,5 +1,10 @@
 dataTaipeiGET <- function(scope=NULL,q=NULL,limit=1000,offset=0, ...) {
   url <- "http://data.taipei/opendata/datalist/apiAccess"
+  if (!is.null(q)) {
+    if (Sys.info()["sysname"] %>% tolower == "windows") {
+      q <- iconv(q, from = "BIG-5", to = "UTF-8")
+    }
+  }
   response <- httr::GET(url,query=list(scope=scope,q=q,limit=limit,offset=offset, ...)) 
   dataTaipeiCheck(response)
   response
