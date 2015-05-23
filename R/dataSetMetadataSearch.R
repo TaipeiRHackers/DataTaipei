f9bb82980b84c050cb56da3df6b9b4c56db57a45
John Chen <john.data.chen@facebook.com>
2015-05-23 14:40:03 +0800

    add and update comments of 3 functions.

--- R/dataSetMetadataSearch.R
+++ R/dataSetMetadataSearch.R
@@ -1,15 +1,22 @@
-#'@title dataSetMetadataSearch
-#'@param q (character). Keyword of datasets. Read the details for more information.
-#'@param limit (integer). Limit quanty of returned records to set a proper scale of data output.
-#'@param offset (integer). The start position to show of query output.  read the details for more information.
-#'@details This is the example of q advanced usage. Such as q="title:youbike cost:free", which will return the result's title contains "youbike" and cost contains "free". The method is "condition-1 condition-2 ...", which uses one blank space to separate each condition, and you can input one or more conditions to query.
+#'@title Search Dataset of \url{http://data.taipei} via Metadata
+#'@param q (character). Keyword of the datasets. Please read the details for more information.
+#'@param limit (integer). Limit the number of returned records.
+#'@param offset (integer). The start position of the returned records.  Please read the details for more information.
+#'@details 
+#'  You could directly put the keyword into \code{q}. For example, \code{dataSetMetadataSearch(q = "youbike")}. 
+#'  \code{q} also accept advanced usage such as \code{q="title:youbike cost:free"} which will return the records whose title contains "youbike" and cost contains "free". The schema is "field-1:condition-1 field-2:condition-2 ...". The blank space separates each condition. For more details, please check the reference.
 #'
-#'Please read the references for more information.
-#'   
-#'   This is the example of offset usage. Such as offset = 5, which will return the 1 + 5 = 6th dataset. 
-#'   
-#'   Please read the references for more information.
+#'  For {offset}, here is an example. If \code{offset = 5}, the \code{dataSetMetadataSearch} will return the \code{1 + 5 = 6}th row. 
+#'
+#'@return A data.table. A table of related datasets.
+#'@seealso \code{\link{getResources}}
 #'@references \url{http://tpeodck.gitbooks.io/data-taipei-developer-guide/content/chapter2section1.html}
+#'@example
+#'\dontrun{
+#'rs <- dataSetMetadataSearch(q = "youbike")
+#'rid <- getResources(rs, 1)
+#'suppressWarnings(df <- resourceAquire(rid$resourceId[1]))
+#'}
 #'@export
 dataSetMetadataSearch <- function(q=NULL,limit=1000,offset=0) {
   response <- dataTaipeiGET("datasetMetadataSearch",q,limit,offset)
--- R/resourceAquire.R
+++ R/resourceAquire.R
@@ -1,7 +1,14 @@
-#'@title resourceAquire
-#'@param resourceId which is one column of query result by using getResources(). Please read seealso to get more information.
+#'@title Download the Resource from \url{http://data.taipei}
+#'@param resourceId (character). The id of the resource.
 #'@seealso \code{\link{getResources}}
 #'@references \url{http://tpeodck.gitbooks.io/data-taipei-developer-guide/content/chapter2section3.html}
+#'@return A data.frame. The downloaded table of the resource.
+#'@example
+#'\dontrun{
+#'rs <- dataSetMetadataSearch(q = "youbike")
+#'rid <- getResources(rs, 1)
+#'suppressWarnings(df <- resourceAquire(rid$resourceId[1]))
+#'}
 #'@export
 resourceAquire <- function(resourceId) {
   response <- dataTaipeiGET(scope = "resourceAquire", rid = resourceId, format = "csv")
