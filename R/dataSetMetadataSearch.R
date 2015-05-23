14b6be53a34145130f50e6c4dff9fc25c3697cbe
Wush Wu <wush978@gmail.com>
2015-05-23 09:29:07 +0800

    #17

--- R/dataSetMetadataSearch.R
+++ R/dataSetMetadataSearch.R
@@ -1,3 +1,5 @@
+#'@param q (character). 
+#'@param limit (integer). Limit of returned records.
 #'@export
 dataSetMetadataSearch <- function(q=NULL,limit=1000,offset=0) {
   response <- dataTaipeiGET("datasetMetadataSearch",q,limit,offset)
