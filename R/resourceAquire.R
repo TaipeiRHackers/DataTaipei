8ec27c5ba2ece77f16363f4c50af7805b53389a0
John Chen <john.data.chen@facebook.com>
2015-05-23 10:47:52 +0800

    add resourceAquire introductions

--- R/resourceAquire.R
+++ R/resourceAquire.R
@@ -1,6 +1,9 @@
+#'@title resourceAquire
+#'@param resourceId which is one column of query result by using getResources(). Please read seealso to get more information.
+#'@seealso getResources()
 #'@export
 resourceAquire <- function(rid) {
-  response <- dataTaipeiGET(scope = "resourceAquire", rid = rid, format = "csv")
+  response <- dataTaipeiGET(scope = "resourceAquire", resourceId = resourceId, format = "csv")
   dataTaipeiCheck(response)
   resourceParse(response)
   
