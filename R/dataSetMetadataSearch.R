eeaa0b46bb70ebb30b684afc2e01e3de9f8066e1
John Chen <john.data.chen@facebook.com>
2015-05-23 14:11:17 +0800

    updated the comments of 3 functions.

--- R/dataSetMetadataSearch.R
+++ R/dataSetMetadataSearch.R
@@ -1,12 +1,14 @@
 #'@title dataSetMetadataSearch
 #'@param q (character). Keyword of datasets. Read the details for more information.
 #'@param limit (integer). Limit quanty of returned records to set a proper scale of data output.
-#'@param offset (integer). The start position to show of query output.  see the details for more information.
-#'@details This is the example of q advanced usage. Such as q="title:youbike cost:free", which will return the result's title contains "youbike" and cost contains "free". The method is "condition-1 condition-2 ...", which uses one blank space to separate each condition, and you can input one or more conditions to query. Please see the references for more information.
+#'@param offset (integer). The start position to show of query output.  read the details for more information.
+#'@details This is the example of q advanced usage. Such as q="title:youbike cost:free", which will return the result's title contains "youbike" and cost contains "free". The method is "condition-1 condition-2 ...", which uses one blank space to separate each condition, and you can input one or more conditions to query.
+#'
+#'Please read the references for more information.
 #'   
 #'   This is the example of offset usage. Such as offset = 5, which will return the 1 + 5 = 6th dataset. 
 #'   
-#'   Please see the references for more information.
+#'   Please read the references for more information.
 #'@references \url{http://tpeodck.gitbooks.io/data-taipei-developer-guide/content/chapter2section1.html}
 #'@export
 dataSetMetadataSearch <- function(q=NULL,limit=1000,offset=0) {
--- R/resourceMetadataSearch.R
+++ R/resourceMetadataSearch.R
@@ -1,10 +1,14 @@
 #'@title resourceMetadataSearch
-#'@param q (character). Keyword of datasets, what you want to know. Please see the details for more information.
-#'@param limit (integer). Limit of returned records, if you just need the part of results.
-#'@param offset (integer). Distance of the starting point. Please see the details for more information.
-#'@details This is the example of q advanced usage. Such as q=title:youbike cost:free, which will return the result's title contains "youbike" and cost contains "free". The method is "condition-1 condition-2 ...", which uses one blank space to separate each condition, and you can input two and more conditions to query. Please see the references for more information.
-#'  
-#'  This is the example of offset usage. Such as offset = 5, which will return the 1 + 5 = 6th dataset. Please see the references for more information.
+#'@param q (character). Keyword of datasets. Read the details for more information.
+#'@param limit (integer). Limit quanty of returned records to set a proper scale of data output.
+#'@param offset (integer). The start position to show of query output.  read the details for more information.
+#'@details This is the example of q advanced usage. Such as q="title:youbike cost:free", which will return the result's title contains "youbike" and cost contains "free". The method is "condition-1 condition-2 ...", which uses one blank space to separate each condition, and you can input one or more conditions to query.
+#'
+#'Please read the references for more information.
+#'   
+#'   This is the example of offset usage. Such as offset = 5, which will return the 1 + 5 = 6th dataset. 
+#'   
+#'   Please read the references for more information.
 #'@references \url{http://tpeodck.gitbooks.io/data-taipei-developer-guide/content/chapter2section2.html}
 #'@export
 resourceMetadataSearch <- function(q=NULL,limit=1000,offset=0) {
