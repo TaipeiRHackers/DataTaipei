a66610b8a559947cc3ab6075aae9e052f7f7fda3
will121 <will121@gmail.com>
2015-05-28 21:47:22 +0800

    add email choice and dataTaipeiEmail()

--- R/DataTaipei.R
+++ R/DataTaipei.R
@@ -12,7 +12,25 @@ dataTaipeiGET <- function(scope=NULL,q=NULL,limit=1000,offset=0,sort=NULL, ...) 
 
 dataTaipeiCheck <- function(response) {
   if(response$status==500){
+    print("Email the error message to Taipei City Government?")
+    input<-readline("Please give your choice: Y or N ")
+    if(input=="Y"){
+      account<-readline("Please give your Gmail account name :")
+      pwd<-readline("Please give your Gmail password :")
+      dataTaipeiEmail(account,pwd)
+    }
     stop("Connection to Taipei City Government Open Data Plateform failed, please try again.")
   }
+}
+
+dataTaipeiEmail <- function(account=NULL,pwd=NULL){
+  mailR::send.mail(from = paste0(account,"@gmail.com"),
+            to = "taipeirhacker@gmail.com", # change to services@mail.taipei.gov.tw when confirmed
+            subject = "Taipei Open Data API Connection failed",
+            body = "Dear Sir, there is a problem for your reference that Taipei City Open Data API (resourceAquire) didn't work. ",
+            smtp = list(host.name = "smtp.gmail.com", port = 465, user.name = account, passwd = pwd , ssl = TRUE),
+            authenticate = TRUE,
+            send = TRUE)
+  print("the email has sent !")
 }
 
